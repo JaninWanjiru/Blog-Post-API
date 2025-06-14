b956be80fa4a69e965ade51a57db54f47fbc85db
JaninWanjiru <janinndongo02@gmail.com>
2025-06-14 16:41:55 +0300

    add port configuration and test it

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import express from 'express';
 
 const app = express()
+
+app.get("/", (req, res) => {  // A landing page of our API to test if our port configuration is working.
+    res.send("<h1>You've unlocked the Blog post API</h1>")
+})
+
+
+// Starting server/ Port configuration
+const port = process.env.PORT || 3300;
+app.listen(port, () =>{
+    console.log(`App is running on port ${port}` );
+});
