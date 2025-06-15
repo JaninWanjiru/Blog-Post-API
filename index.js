be4f85d01c166d520d0dcef4a076b94e8d7a911c
JaninWanjiru <janinndongo02@gmail.com>
2025-06-15 12:37:38 +0300

    add a route to create a user

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import { PrismaClient } from '@prisma/client';
 const app = express()
 const client = new PrismaClient()
 
+app.use(express.json())
+
 
 app.get("/", (_req, res) => {  // A landing page of our API to test if our port configuration is working.
     res.send("<h1>You've unlocked the Blog post API</h1>")
@@ -16,10 +18,26 @@ app.get("/users", async(req, res) =>{
       res.status(200).json(users)
     } catch (e) {
       res.status(500).json({message: "Something went wrong"})
-    }
+    } 
 }) 
 
-
+// Create a user
+app.post("/users", async (req, res) => {
+  try {
+    const {firstName, lastName, emailAddress, userName} = req.body;
+    const newUser = await client.user.create({
+      data: {
+        firstName,
+        lastName,
+        emailAddress,
+        userName
+      }
+    })
+    res.status(201).json(newUser)
+  } catch (e) {
+    res.status(500).json({message: "Something went wrong"})
+  }
+})
 
 
 
